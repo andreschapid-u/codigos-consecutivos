b4ab0668479747ee592c2aa20962fe435273fa9c
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2018-09-30 18:04:26 -0500

    Terminado por mi parte

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 // URL DEL SCRIPT SERVER
 const urlServer = './server.php';
 
-function ajax(metodos, url, funcionEjecutar) {
+function ajax(metodos, url, funcionEjecutar, datos = '') {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == XMLHttpRequest.DONE) {
             if (this.status == 200) {
                 funcionEjecutar(JSON.parse(this.responseText));
                 // funcionEjecutar(this.responseText);
-            } else {
-
-            }
+            } else {}
         }
     };
     xmlhttp.open(metodos, url, true);
-    xmlhttp.send();
+    xmlhttp.send(datos);
 }
 
 function log(data) {
@@ -23,7 +21,6 @@ function log(data) {
 
 // ajax('GET', urlServer, log);
 ajax('GET', urlServer, llenarDependencias);
-
 
 // CARGA CON DATOS EL SELECT DE DEPENDENCIAS
 function llenarDependencias(datos) {
@@ -45,20 +42,23 @@ function llenarDependencias(datos) {
 }
 
 function llenarSubDependencias(datos) {
-    var select1 = document.querySelector('#select1').value;
-    log(select1)
-    var select = document.querySelector('#select2');
-    select.innerHTML = '';
-    var option = document.createElement('option');
-    option.text = 'Seleccione';
-    option.value = '-1';
-    select.appendChild(option);
-    var dependencia = document.querySelector("#dep");
+    // log(select1);
     if (datos.status == 1) {
-        // log(datos)
-        dependencia.style.backgroundColor = "#" + datos.datos.color;
-        dependencia.style.color = "#ffffff";
+        var select1 = document.querySelector('#select1').value;
+        var select = document.querySelector('#select2');
+        select.innerHTML = '';
+        var option = document.createElement('option');
+        option.text = 'Seleccione';
+        option.value = '-1';
+        select.appendChild(option);
+        var dependencia = document.querySelector('#dep');
+        var subdependencia = document.querySelector('#subDep');
+
+        dependencia.style.backgroundColor = '#' + datos.datos.color;
+        dependencia.style.color = '#ffffff';
         dependencia.innerHTML = select1;
+        subdependencia.style.backgroundColor = 'transparent';
+        subdependencia.innerHTML = 'Elegir';
         // dependencia.sty = datos.datos.color;
         var sub_dependencias = datos.datos.sub_dependencias;
         for (const x in sub_dependencias) {
@@ -71,31 +71,86 @@ function llenarSubDependencias(datos) {
             // console.log(dep.codigo);
         }
     } else {
-
+        mostrarError(datos.mensaje);
     }
 }
 
+function elegirSubDependencia(datos) {
+    if (datos.status == 1) {
+        var select = document.querySelector('#select2');
+        var subdependencia = document.querySelector('#subDep');
+        subdependencia.innerHTML = select.value;
+        subdependencia.style.backgroundColor = '#' + datos.datos.color;
+        subdependencia.style.color = '#ffffff';
+    } else {
+        mostrarError(datos.mensaje);
+    }
+}
 
 document.querySelector('#select1').addEventListener('change', function() {
     if (this.value != -1) {
         console.log(this.value);
         // ajax('GET', urlServer + '?cod_dependencia=' + this.value, log);
         ajax('GET', urlServer + '?cod_dependencia=' + this.value, llenarSubDependencias);
+    } else {
+        var dependencia = document.querySelector('#dep');
+        var subdependencia = document.querySelector('#subDep');
+        dependencia.innerHTML = 'Elegir';
+        dependencia.style.backgroundColor = 'transparent';
+        dependencia.style.color = '#000';
+        subdependencia.innerHTML = 'Elegir';
+        subdependencia.style.backgroundColor = 'transparent';
+        subdependencia.style.color = '#000';
+        var select = document.querySelector('#select2');
+        select.innerHTML = '';
+        var option = document.createElement('option');
+        option.text = 'Seleccione';
+        option.value = '-1';
+        select.appendChild(option);
+        document.querySelector('#generado').innerHTML = 'Generar';
     }
 });
 
 document.querySelector('#select2').addEventListener('change', function() {
     if (this.value != -1) {
-        var dep = document.querySelector("#select1").value;
+        var dep = document.querySelector('#select1').value;
         console.log(this.value);
-        ajax('GET', urlServer + '?cod_subdependencia=' + this.value + '&dependencia=' +
-            dep, log);
+        ajax('GET', urlServer + '?cod_subdependencia=' + this.value + '&dependencia=' + dep, elegirSubDependencia);
         // ajax('GET', urlServer + '?cod_subdependencia=' + this.value + '&dependencia=' +
         //     dep, log);
+    } else {
+        var subdependencia = document.querySelector('#subDep');
+        subdependencia.innerHTML = 'Elegir';
+        subdependencia.style.backgroundColor = 'transparent';
+        subdependencia.style.color = '#000';
+        document.querySelector('#generado').innerHTML = 'Generar';
     }
 });
 
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    ajax('POST', urlServer, log);
+    var dep = document.querySelector('#select1').value;
+    var subDep = document.querySelector('#select2').value;
+    if (dep == -1 || subDep == -1) {
+        mostrarError('Debe seleccionar una dependecia y subdepencia primero!!');
+    } else {
+        ajax('POST', urlServer, consecutivoGenerado);
+    }
 });
+
+function consecutivoGenerado(datos) {
+    if (datos.status == 1) {
+        document.querySelector('#generado').innerHTML = datos.datos;
+        log(datos.datos.datos);
+    } else {
+        mostrarError(datos.mensaje);
+    }
+}
+
+function mostrarError(mensaje) {
+    document.querySelector('#error').innerHTML = mensaje;
+    document.querySelector('#error').style.display = 'block';
+    setTimeout(() => {
+        document.querySelector('#error').style.display = 'none';
+    }, 4500);
+}
