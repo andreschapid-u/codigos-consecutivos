32052e97dfbb2fe903a6b541ab8db654e9f5e8a9
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2018-09-30 08:53:14 -0500

    Verificacion y funciones de  manejos de rchivos

--- server.php
+++ server.php
@@ -1,23 +1,31 @@
 <?php
+header('Content-type: application/json; charset=utf-8');
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+
 $arc_consecutivo="consecutivo.json";
 $arc_dependencias="dependencias.json";
 
-class SubDependencia{
+class SubDependencia
+{
     public $codigo;
     public $nombre;
     public $color;
-    public function __construct($codigo, $nombre, $color) {
+    public function __construct($codigo, $nombre, $color)
+    {
         $this->codigo = $codigo;
         $this->nombre = $nombre;
         $this->color = $color;
     }
 }
-class Dependencia extends SubDependencia{
+class Dependencia
+{
     public $codigo;
-	public $nombre;
+    public $nombre;
     public $color;
     public $sub_dependencias;
-    public function __construct($codigo, $nombre, $color, $sub_dependencias) {
+    public function __construct($codigo, $nombre, $color, $sub_dependencias)
+    {
         $this->codigo = $codigo;
         $this->nombre = $nombre;
         $this->color = $color;
@@ -25,25 +33,35 @@ class Dependencia extends SubDependencia{
     }
 }
 
-if(!file_exists($arc_consecutivo)){
+// verificamos si el archivo no existe o esta vacio
+if (!file_exists($arc_consecutivo) || filesize($arc_consecutivo) == 0) {
     $codigo = new stdClass();
     $codigo->valor = 0;
     file_put_contents($arc_consecutivo, json_encode($codigo));
 }
 
-if(!file_exists($arc_dependencias)){
+// verificamos si el archivo no existe o esta vacio
+if (!file_exists($arc_dependencias) || filesize($arc_consecutivo) == 0) {
     $archivo = new stdClass();
     $archivo->dependencias=[
-        new Dependencia(1,"Decanatura","0000080",0),
-        new Dependencia(4,"Departamento de Sistemas","800080",1),
-        new Dependencia(5,"Departamento de Electrónica","008080",1)
+        new Dependencia(1, "Decanatura", "0000080", 0),
+        new Dependencia(4, "Departamento de Sistemas", "800080", 1),
+        new Dependencia(5, "Departamento de Electrónica", "008080", 1)
     ];
     $archivo->sub_dependencias=[[
-        new SubDependencia(1,"Decano","800000"),
-        new SubDependencia(2,"Secretaría General","9999ff")
+        new SubDependencia(1, "Decano", "800000"),
+        new SubDependencia(2, "Secretaría General", "9999ff")
     ],[
-        new SubDependencia(1,"Jefe de Departamento","808000"),
-        new SubDependencia(1,"Tesorería","ffff00")
+        new SubDependencia(1, "Jefe de Departamento", "808000"),
+        new SubDependencia(1, "Tesorería", "ffff00")
     ]];
     file_put_contents($arc_dependencias, json_encode($archivo));
+}
+
+// FUNCIONES LECTURA ESCRITURA ARCHIVOS
+function leerArchivo($nombreArchivo){
+    return json_decode(file_get_contents($nombreArchivo));
+}
+function escribirArchivo($nombreArchivo,$datos){
+    file_put_contents($nombreArchivo, json_encode($datos));
 }
